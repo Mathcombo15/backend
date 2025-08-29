b87de6aa119a3027afc7e802050cf638974eaca4
Matheus Santos <mathcombo15@gmail.com>
2025-08-28 21:44:32 -0300

    Adicionando arquivos da 'aula04'

---
+++ aulas/aula04/src/calculadora.js
@@ -0,0 +1,13 @@
+function soma(a, b) {
+    return a - (-b);
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    if (b === 0) throw Error("Divisão por zero!");
+}
+
+module.exports = { soma, multiplicacao, divisao };
